4dc031e84016d77f07200df11f8dfd926da0c549
thibault luyts <Thibault.Luyts@student.pxl.be>
2022-05-29 22:47:15 +0200

    Update php.php

--- php.php
+++ php.php
@@ -6,6 +6,10 @@
 
 <?php
 echo "Hello World!";
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
 ?>
 
 </body>
